ed2bbfcfdae17994c066ba07dbe611fa59c57453
kohziyi95 <96542810+kohziyi95@users.noreply.github.com>
2022-03-10 20:19:21 +0800

    new

--- src/main/java/sg/edu/nus/iss/library/config/RedisConfig.java
+++ src/main/java/sg/edu/nus/iss/library/config/RedisConfig.java
@@ -28,8 +28,8 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private Optional<Integer> redisPort;
 
-    // @Value("${spring.redis.password}")
-    // private String redisPassword;
+    @Value("${spring.redis.password}")
+    private String redisPassword;
 
     @Bean
     @Scope("singleton")
@@ -40,7 +40,7 @@ public class RedisConfig {
 
         config.setHostName(redisHost);
         config.setPort(redisPort.get());
-        // config.setPassword(redisPassword);
+        config.setPassword(redisPassword);
 
         final JedisClientConfiguration jedisClient = JedisClientConfiguration.builder().build();
         final JedisConnectionFactory jedisFac = new JedisConnectionFactory(config, jedisClient);
